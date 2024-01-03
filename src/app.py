a9622ca83deb767b50bf8cf10187b2b8363e39a5
Tomas Enrique Cevallos <tecevallos@hotmail.com>
2024-01-02 23:13:53 -0500

    Converted CSV to json

--- src/app.py
+++ src/app.py
@@ -1,17 +1,14 @@
 from flask import Flask, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
 # Sample data for players
-players_data = [
-    {"id": 1, "name": "Player 1", "score": 100},
-    {"id": 2, "name": "Player 2", "score": 150},
-    {"id": 3, "name": "Player 3", "score": 120},
-]
+df = pd.read_csv("premierLeagueDatabase.csv")
 
 @app.route('/getplayers', methods=['GET'])
 def get_players():
-    return jsonify(players_data)
+    return df.to_json()
 
 if __name__ == '__main__':
     app.run(debug=True)
